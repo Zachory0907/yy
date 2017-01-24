8e0afeba0ae5cde6d6adcbd4bb2a18105db22f0c
Zachory0907 <13653643393@163.com>
2017-01-24 13:34:14 +0800

    # hello world 开发完成

--- myapps/src/main/java/vip/zgt/app/test/HelloController.java
+++ myapps/src/main/java/vip/zgt/app/test/HelloController.java
@@ -4,4 +4,8 @@ import com.jfinal.core.Controller;
 
 public class HelloController extends Controller{
 
+	public void index (){
+		System.out.println("hello!");
+		renderText("Hello World!");
+	}
 }
